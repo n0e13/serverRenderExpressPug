bab397e6663254962efff8a0c3207ada5a47bfb2
n0e <noemy.garcia@gmail.com>
2022-03-10 14:25:34 +0100

    [ADD] All folders and files

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const port = process.env.PORT;
 
 require('dotenv').config();
 
+// Permite recibir parámetros en formato JSON.
+app.use(express.json());
+app.use(express.static('public'));
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+
+
 
 
 app.listen(port, () => {
